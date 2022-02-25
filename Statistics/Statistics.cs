aa8048e7d75f3df8ee45b696268d1a8492a0c066
Anitha-Bhat <anithabhat97@gmail.com>
2022-02-25 16:02:19 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -1,24 +1,79 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Statistics
 {
-  public class StatsComputer
+  public class StatsComputer:Stats
   {
-    public Stats CalculateStatistics(List<float> numbers)
+    
+    public Stats CalculateStatistics(List<double> numbers)
     {
-      Stats objStats=new Stats();
-      objStats.average=numbers.average()
-      objStats.max = numbers.max();
-      objStats.min = numbers.min();
-      return objStats;
+      Stats s = new Stats();
+      if (numbers.Count > 0)
+      {
+      
+        average = numbers.Average();
+        max = numbers.Max();
+        min = numbers.Min();
+      }
+     
+     return s;
     }
   }
   public class Stats
   {
-    private double average;
-    private double max;
-    private double min;
+   public double average=Double.NaN;
+   public double max= Double.NaN;
+   public double min= Double.NaN;
 
+  }
+  public interface IAlerter
+  {
+    void Alert();
+  }
+  public class EmailAlert : IAlerter
+  {
+    public bool emailSent = false;
+
+
+    public void Alert()
+    {
+      emailSent = true;
+    }
+  }
+
+  public class LEDAlert : IAlerter
+  {
+    public bool ledGlows = false;
+    public void Alert()
+    {
+      ledGlows = true;
+    }
+  }
+
+ 
+  public class StatsAlerter
+  {
+    private double threshold;
+    private IAlerter[] alerters;
+
+   public StatsAlerter(double maxThreshold,IAlerter[] alerter)
+   {
+     threshold = maxThreshold;
+     alerters = alerter;
+
+   }
+   public void checkAndAlert(List<double> numbers)
+    {
+      if (numbers.Max() > threshold)
+      {
+        foreach (var alert in alerters)
+        {
+          alert.Alert();
+
+        }
+      }
+    }
   }
 }
